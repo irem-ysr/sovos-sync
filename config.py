a7a64ee176f5c565745749b01cd704a286b7bd4e
Irem Yasar <irem.yasar@boun.edu.tr>
2026-04-01 18:56:56 +0300

    first commit

--- config.py
+++ config.py
@@ -3,14 +3,14 @@ from pathlib import Path
 WSDL_URL = "https://integrationservicewithoutmtom.digitalplanet.com.tr/IntegrationService.asmx?WSDL"
 SERVICE_URL = "https://integrationservicewithoutmtom.digitalplanet.com.tr/IntegrationService.asmx"
 
-CORPORATE_CODE = "MZYCAFETAR"
-LOGIN_NAME = "wsadminuser"
-PASSWORD = "d0v+.Gm-x5"
+CORPORATE_CODE = ""
+LOGIN_NAME = ""
+PASSWORD = ""
 
-GOOGLE_SERVICE_ACCOUNT_FILE = Path("/Users/PC/Desktop/sovos-api/google-service-account.json")
+GOOGLE_SERVICE_ACCOUNT_FILE = Path("google-service-account.json")
 SPREADSHEET_NAME = "Sovos Fatura Takibi"
 OUTGOING_SHEET_NAME = "Giden Faturalar"
 INCOMING_SHEET_NAME = "Gelen Faturalar"
 
-LOOKBACK_DAYS = 30
+LOOKBACK_DAYS = 3
 SOAP_TIMEOUT_SECONDS = 300
